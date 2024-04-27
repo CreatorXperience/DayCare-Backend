b6383dc16f25f871c2e304508043e941acfc2fea
CreatorXperience <allyearmustobey@gmail.com>
2024-04-27 07:23:19 +0000

    ch-#11: update socket origin

--- socket/index.ts
+++ socket/index.ts
@@ -8,7 +8,7 @@ let onlineUsers: Array<{userId: string, socketId: string}> = []
 const socketConnection = ()=> {
     let httpServer = http.createServer(app)
 
-      let environment = process.env.NODE_ENV == "production" ? "https://daycare-app-frontend-git-ch-u-1ce67c-creatorxperiences-projects.vercel.app" : "http://localhost:3000"
+      let environment = process.env.NODE_ENV == "production" ? "https://daycare-app-frontend-1g76ynpox-creatorxperiences-projects.vercel.app" : "http://localhost:3000"
 
     let io  = new Server(httpServer, {
      cors: {
